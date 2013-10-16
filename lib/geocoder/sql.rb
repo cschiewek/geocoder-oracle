d15fdce7cbd855ab0e94079f880c03dd4482f9a8
Curtis Schiewek <curtis.schiewek@greatersudbury.ca>
2013-10-16 14:56:48 -0400

    added overwridden full_bearing method

---
+++ lib/geocoder/sql.rb
@@ -0,0 +1,42 @@
+module Geocoder
+  module Sql
+    undef_method :full_bearing
+    # extend self
+    # ##
+    # # Fairly accurate bearing calculation. Takes a latitude, longitude,
+    # # and an options hash which must include a :bearing value
+    # # (:linear or :spherical).
+    # #
+    # # Based on:
+    # # http://www.beginningspatial.com/calculating_bearing_one_point_another
+    # #
+    # # Overridden for Oracle as Oracle has no support for modulo % operator.
+    # ##
+    # def full_bearing(latitude, longitude, lat_attr, lon_attr, options = {})
+    #   binding.pry
+    #   case options[:bearing] || Geocoder.config.distances
+    #   when :linear
+    #     "MOD(" +
+    #     "CAST(" +
+    #       "DEGREES(ATAN2( " +
+    #         "RADIANS(#{lon_attr} - #{longitude.to_f}), " +
+    #         "RADIANS(#{lat_attr} - #{latitude.to_f})" +
+    #       ")) + 360 " +
+    #     "AS decimal), 360)"
+    #   when :spherical
+    #     "MOD(" +
+    #     "CAST(" +
+    #       "DEGREES(ATAN2( " +
+    #         "SIN(RADIANS(#{lon_attr} - #{longitude.to_f})) * " +
+    #         "COS(RADIANS(#{lat_attr})), (" +
+    #           "COS(RADIANS(#{latitude.to_f})) * SIN(RADIANS(#{lat_attr}))" +
+    #         ") - (" +
+    #           "SIN(RADIANS(#{latitude.to_f})) * COS(RADIANS(#{lat_attr})) * " +
+    #           "COS(RADIANS(#{lon_attr} - #{longitude.to_f}))" +
+    #         ")" +
+    #       ")) + 360 " +
+    #     "AS decimal), 360)"
+    #   end
+    # end
+  end
+end
