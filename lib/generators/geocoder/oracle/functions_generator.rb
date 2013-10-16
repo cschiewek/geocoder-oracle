e81aabbd685727f62e66f9a50365b87f71781a9e
Curtis Schiewek <curtis.schiewek@greatersudbury.ca>
2013-10-16 15:34:40 -0400

    added other oracle functions to generator

--- lib/generators/geocoder/oracle/functions_generator.rb
+++ lib/generators/geocoder/oracle/functions_generator.rb
@@ -6,6 +6,7 @@ module Geocoder
       ActiveRecord::Base.establish_connection
 
       desc 'Creates a custom functions required for geocoding in the Oracle database'
+
       def pi_function
         function = %q(
           CREATE OR REPLACE FUNCTION PI
@@ -17,6 +18,35 @@ module Geocoder
           END PI;
         )
         ActiveRecord::Base.connection().execute(function)
+        puts 'PI() function added.'
+      end
+
+      def degrees_function
+        function = %q(
+          CREATE OR REPLACE FUNCTION DEGREES(RADIAN IN NUMBER)
+            RETURN NUMBER DETERMINISTIC IS
+            DEGREES NUMBER;
+          BEGIN
+            SELECT RADIAN * 57.2957795 INTO DEGREES FROM DUAL;
+            RETURN DEGREES;
+          END DEGREES;
+        )
+        ActiveRecord::Base.connection().execute(function)
+        puts 'DEGREES(RADIAN) function added.'
+      end
+
+      def radians_function
+        function = %q(
+          CREATE OR REPLACE FUNCTION RADIANS(DEGREE IN NUMBER)
+            RETURN NUMBER DETERMINISTIC IS
+            RADIANS NUMBER;
+          BEGIN
+            SELECT DEGREE / 57.2957795 INTO RADIANS FROM DUAL;
+            RETURN RADIANS;
+          END RADIANS;
+        )
+        ActiveRecord::Base.connection().execute(function)
+        puts 'RADIANS(DEGREE) function added.'
       end
     end
   end
